c619835c15e5ddb15d635051462f7a2f0b97f41e
yuzhenliu <1109763686@qq.com>
2019-10-06 10:36:47 +0800

    写好了登录注册的逻辑

--- static/js/login.js
+++ static/js/login.js
@@ -13,10 +13,10 @@
 
 
   // 用户注册登录事件
-  let $username = $('.formWrap #username'); // 账户
-  let $password = $('.formWrap #password'); // 密码
-  let $loginBtn = $('.formWrap .loginBtn') // 登录按钮
-  let $rememberMe = $('.formWrap #rememberMe'); // 记住我
+  let $username = $('.loginTabCon #username'); // 账户
+  let $password = $('.loginTabCon #password'); // 密码
+  let $loginBtn = $('.loginTabCon .loginBtn') // 登录按钮
+  let $rememberMe = $('.loginTabCon #rememberMe'); // 记住我
 
   // 当用户来到页面时，首先判断本地 cookie 中有没有对应的用户数据如
   // document.cookie = 'username=yuzhenliu;password=123456'
@@ -66,35 +66,36 @@
         pass: password
       })
       .then((data) => {
-        if (data.err == 0) {
-          // 登录成功，可以执行登录成功的操作了
 
-          console.log($rememberMe.prop('checked'));
+        switch (data.err) {
+          case 0:
+            // 登录成功，可以执行登录成功的操作了
 
-          if ($rememberMe.prop('checked')) { // 记住我为 true
-            setCookie('us', username, 7);
-            $username.val('');
-            $password.val('');
-            console.log('进入了添加');
+            console.log($rememberMe.prop('checked'));
 
-          }else {
-            removeCookie('us');
-            $username.val('');
-            $password.val('');
-            console.log('删除');
-          }
+            if ($rememberMe.prop('checked')) { // 记住我为 true
+              setCookie('us', username, 7);
+              $username.val('');
+              $password.val('');
+              $rememberMe.prop('checked', false);
 
-          alert(data.msg);
+            } else {
+              removeCookie('us');
+              $username.val('');
+              $password.val('');
+              $rememberMe.prop('checked', false);
+            }
 
-        }else if (data.err == -1) {
-          alert(data.msg); // 参数错误 少了参数 比较少出现
-        }else if (data.err == -2) {
-          alert(data.msg); // 用户名或密码不正确
+            alert(data.msg);
+            break;
+          case -1:
+            alert(data.msg); // 参数错误 少了参数 比较少出现
+            break;
+          case -2:
+            alert(data.msg); // 用户名或密码不正确
+            break;
         }
 
-      }, (err) => {
-        console.log(err);
-        console.log('请求出错');
       })
       .catch((err) => {
         console.log(err);
@@ -107,7 +108,70 @@
 
 
 
+  // 用户注册事件
+  let $usernameReg = $('.joinMembers #usernameReg'); // 账户
+  let $passwordReg = $('.joinMembers #passwordReg'); // 密码
+  let $registerBtn = $('.joinMembers .registerBtn') // 登录按钮
 
+  $registerBtn.click(function () {
+
+    if ($usernameReg.val() == '') {
+      alert('账户不可为空');
+      return
+    }
+
+    if ($passwordReg.val() == '') {
+      alert('密码不可为空');
+      return
+    }
+
+    // 拿到值，发送 ajax，看数据库中是否有对应的数据，
+    // post
+    // url: localhost:3000/register
+    // data: us   pass
+    // 返回: {err: 0, msg: '用户注册成功'}
+    // 参数不完整: {"err": -1,"msg": "参数错误"}
+    // res.send({err: -2, msg: '注册失败'})
+    // {err: -3, msg: '该用户名已存在'}
+    // {err: -5, msg: '账户格式不正确,必须为手机号或者邮箱'}
+
+    let usernameReg = $usernameReg.val();
+    let passwordReg = $passwordReg.val();
+
+    if (!(/^\w{2,18}@[0-9a-z]{1,10}(\.[a-z]{2,3}){1,2}$/.test(usernameReg) || /^(1|\+861)[3-8]{1}\d{9}$/.test(usernameReg))) {
+      alert('账户格式不正确,必须为手机号或者邮箱');
+      return
+    }
+
+    ajaxPromise('post', 'http://localhost:3000/register', {
+        us: usernameReg,
+        pass: passwordReg
+      })
+      .then((data) => {
+        switch (data.err) {
+          case 0: // 处理注册成功的业务逻辑
+            alert(data.msg);
+            break;
+          case -1:
+            alert('参数错误,账号和密码不可为空'); // 比较少，因为做了双重判断
+            break;
+          case -2:
+            alert('注册失败，请稍后重试');
+            break;
+          case -3:
+            alert(data.msg);
+            break;
+          case -5:
+            alert(data.msg);
+            break;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log('catch 中的错误');
+      });
+
+  });
 
 
 
